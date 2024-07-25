8643fc35d75f6abc34ab122ce4e95bcb86f6967b
renancasalle <renan.goncalves@sptech.school>
2024-07-25 14:29:27 -0300

    feat: web content loading and splitting

--- app/travel-agent.py
+++ app/travel-agent.py
@@ -4,6 +4,11 @@ from langchain.agents import create_react_agent, AgentExecutor
 from langchain_community.agent_toolkits.load_tools import load_tools
 from langchain import hub 
 
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorestores import Chroma 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import bs4 
+
 llm  = ChatOpenAI(model="gpt-4o-mini")
 
 query = """
@@ -18,10 +23,25 @@ def researchAgent(query, llm ):
     prompt = hub.pull("hwchase17//react")
     agent = create_react_agent(llm, tools, prompt)
     agent_executor = AgentExecutor(agent = agent, tools = tools, prompt = prompt, verbose = True)
-    webContent = agent_executor.invoke("input" : query)
-    return webContent['output']
+    webContext = agent_executor.invoke({"input" : query})
+    return webContext['output']
 
 print(researchAgent(query, llm))
+
+
+# Carregar conteúdo separado da web e dividir em chunks
+def loadData():
+    loader = WebBaseLoader(
+        webpaths = ("https://www.dicasdeviagem.com/inglaterra"),
+        bs_kwargs = dict(parse_only = bs4.SoupStrainer(class_ =("postcontentwrap", "pagetitle background-imaged loading-dark")))
+    )
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunck_overlap = 200)
+    splits = text_splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(documents = splits, embedding = OpenAIEmbeddings)
+    retriever = vectorstore.as_retriver()
+    return retriever 
+    
 
 #agent_executor.invoke({"input" : query})
 # Vizualizar informações sobre a ferramenta 
